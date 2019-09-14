fa024710ece354b671d1fbfba6cf9cf6a8018747
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-09-14 13:28:43 -0300

    protect timeline route (lesson 3)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import './css/timeline.css';
 import './css/login.css';
 import './css/reset.css';
@@ -8,11 +8,19 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import Login from './components/login';
 
+function verifyAuthentication() {
+  if (!localStorage.getItem('authToken') || !localStorage.getItem('authToken').length) {
+    return <Redirect to="/"></Redirect>
+  } else {
+    return <App/>
+  }
+}
+
 ReactDOM.render(
   (
     <BrowserRouter>
       <Route exact path="/" component={Login}/>
-      <Route exact path="/timeline" component={App}/>
+      <Route exact path="/timeline" render={verifyAuthentication}/>
     </BrowserRouter>
   ),
   document.getElementById('root')
